be1ba411caf5b11fdb962688417486de01071145
Filip <filipmymail@gmail.com>
2025-11-04 02:07:33 +0100

    Put better formatting in tree generator

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -7,26 +7,31 @@ int main() {
     int br;
     cout << "Unesite broj jelki: ";
     cin >> br;
+
     int num;
     cout << "Unesite broj redova: ";
     cin >> num;
 
     int x;
-    for (int k = 0;k < br;k++) {
-        for (int i = 0;i < num;i++) {
-            for (int j = i;j < num - k + br - 2;j++)
+    for (int k = 0; k < br; k++) {
+        for (int i = 0; i < num; i++) {
+            for (int j = i; j < num-k+br-2; j++)
                 cout << ' ';
-            for (x = 0;x <= i + k;x++)
+
+            for (x = 0; x <= i + k; x++)
                 cout << "# ";
+
             cout << endl;
         }
     }
 
-    for (int k = 0;k < (num + br - 1) / 2;k++) {
-        for (int i = 0;i < (num + br) / 2;i++)
+    for (int k = 0; k < (num+br-1) / 2; k++) {
+        for (int i = 0; i < (num + br) / 2; i++)
             cout << ' ';
-         for (int y = 0;y < x / 2;y++)
+
+         for (int y = 0; y < x / 2; y++)
             cout << "# ";
+
         cout << endl;
     }
 }
